da530ea78b989d23d3627cd4195e47d3958d9c39
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2025-06-13 20:06:55 +0300

    functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+saturdayFun("Roller-scate");
+function mondayWork(activity= "go to the office"){
+    return  `This Monday, I will ${activity}.`
+}
+mondayWork();
+mondayWork("Work from home.");
+
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`
+    };
+
+}
+wrapAdjective("%")("a dedicated programmer");
+wrapAdjective("!!!")("amazing");
+const encouragingPromptFunction = wrapAdjective("~");
+console.log(encouragingPromptFunction("Successful"))
